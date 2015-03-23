1b585063fe577d35424224ad975e893a6b51417d
kevinleclair1 <kevinleclair1@gmail.com>
2015-03-23 13:23:11 -0400

    initial commit

---
+++ wp-content/themes/saintclare/page-home.php
@@ -0,0 +1,18 @@
+<?php 
+
+/*
+  Template Name: Home Page Custom
+*/
+
+get_header();  ?>
+
+<div class="main">
+  <div class="container">
+
+    <div class="content">
+
+    </div> <!-- /,content -->
+  </div> <!-- /.container -->
+</div> <!-- /.main -->
+
+<?php get_footer(); ?>
