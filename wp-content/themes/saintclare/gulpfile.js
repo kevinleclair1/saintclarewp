73e91f7550c50f3f16720cdfc786a878e17b251f
kevinleclair1 <kevinleclair1@gmail.com>
2015-03-23 14:06:05 -0400

    got gulp working

---
+++ wp-content/themes/saintclare/gulpfile.js
@@ -0,0 +1,76 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+ 
+// Styles
+gulp.task('styles', function() {
+    gulp.src('styles/style.scss')
+    .pipe(sass({ style: 'expanded', }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('.'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifycss())
+    .pipe(gulp.dest('.'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+ 
+// Scripts
+gulp.task('scripts', function() {
+  return gulp.src('src/js/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+ 
+// Images
+gulp.task('images', function() {
+  return gulp.src('src/img/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/img'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+ 
+// Clean
+gulp.task('clean', function(cb) {
+    del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], cb)
+});
+ 
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'js', 'img');
+});
+ 
+// Watch
+gulp.task('watch', function() {
+ 
+  // Watch .scss files
+  gulp.watch('styles/*.scss', ['styles']);
+ 
+  // Watch .js files
+  gulp.watch('js/**/*.js', ['scripts']);
+ 
+  // Watch image files
+  gulp.watch('src/img/**/*', ['images']);
+ 
+  // Create LiveReload server
+  livereload.listen();
+ 
+  // Watch any files in dist/, reload on change
+  gulp.watch(['dist/**']).on('change', livereload.changed);
+ 
+});
